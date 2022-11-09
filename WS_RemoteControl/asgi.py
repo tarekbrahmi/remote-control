38a685035a1e638832ae94b86a4d5bef49f3301d
tarekbrahmi <brahmitarek6@gmail.com>
2022-11-10 00:38:28 +0100

    fixing the asgi app

--- WS_RemoteControl/asgi.py
+++ WS_RemoteControl/asgi.py
@@ -1,16 +1,14 @@
-"""
-ASGI config for WS_RemoteControl project.
-
-It exposes the ASGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/3.1/howto/deployment/asgi/
-"""
-
+from .urls import *
 import os
-
+from channels.routing import ProtocolTypeRouter, URLRouter
 from django.core.asgi import get_asgi_application
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'WS_RemoteControl.settings')
 
-application = get_asgi_application()
+django_asgi_app = get_asgi_application()
+application = ProtocolTypeRouter({
+    "http": django_asgi_app,
+    "websocket": URLRouter(
+        websocket_urlpatterns
+    ),
+})
--- WS_RemoteControl/urls.py
+++ WS_RemoteControl/urls.py
@@ -1,21 +1,11 @@
-"""WS_RemoteControl URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from .ws_app.consumers import CommandConsumer
+from django.urls import path, re_path
 
+websocket_urlpatterns = [
+    re_path(r'^command/$',
+            CommandConsumer.as_asgi())
+]
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
