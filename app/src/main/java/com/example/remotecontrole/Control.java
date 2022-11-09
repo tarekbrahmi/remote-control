9b5d737f15238769d643fd27e001a5a5a3f9d081
tarekbrahmi <brahmitarek6@gmail.com>
2022-11-09 17:55:39 +0100

    setup the UI and add textViews

--- app/src/main/java/com/example/remotecontrole/Control.java
+++ app/src/main/java/com/example/remotecontrole/Control.java
@@ -4,11 +4,20 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import io.github.controlwear.virtual.joystick.android.JoystickView;
+
 public class Control extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_control);
+        JoystickView joystick = (JoystickView) findViewById(R.id.joystickView);
+        joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
+            @Override
+            public void onMove(int angle, int strength) {
+                // do whatever you want
+            }
+        });
     }
 }
