79f62c4abec255e3fa0ff8004c87888f9f397927
tarekbrahmi <brahmitarek6@gmail.com>
2022-11-12 23:43:05 +0100

    add popup for ip server address

--- app/src/main/java/com/example/remotecontrole/WSControl.java
+++ app/src/main/java/com/example/remotecontrole/WSControl.java
@@ -2,29 +2,23 @@ package com.example.remotecontrole;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.annotation.SuppressLint;
-import android.bluetooth.BluetoothAdapter;
-import android.bluetooth.BluetoothDevice;
-import android.bluetooth.BluetoothSocket;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.res.Configuration;
 import android.net.wifi.WifiInfo;
 import android.net.wifi.WifiManager;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
-import android.os.Message;
 import android.text.format.Formatter;
 import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import org.json.JSONException;
 import org.json.JSONObject;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.OutputStream;
-import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 
 import io.github.controlwear.virtual.joystick.android.JoystickView;
@@ -38,13 +32,11 @@ import okio.ByteString;
 public class WSControl extends AppCompatActivity {
     LinearLayout mainlinearLayout;
     JoystickView joystick;
+    WebSocket webSocket;
+    String serverIp="192.168.43.181";
     TextView txt_angle_value,txt_decision_value;
     private String deviceName = null;
-    private String deviceAddress;
-    public static Handler handler;
-    public static BluetoothSocket mmSocket;
-    private final static int CONNECTING_STATUS = 1;
-    private final static int MESSAGE_READ = 2;
+
     OkHttpClient client = new OkHttpClient.Builder()
             .readTimeout(0,  TimeUnit.MILLISECONDS)
             .build();
@@ -71,12 +63,39 @@ public class WSControl extends AppCompatActivity {
         WifiInfo wifiInfo = wifiMgr.getConnectionInfo();
         int ip = wifiInfo.getIpAddress();
         String ipAddress = Formatter.formatIpAddress(ip);
+
         //TODO make server ip address entred from an input
-        Request request = new Request.Builder().url("ws://192.168.43.181:8000/command/").build();
-        Log.i("WS","request from address "+ipAddress);
-        EchoWebSocketListener listener = new EchoWebSocketListener();
-        WebSocket ws = this.client.newWebSocket(request, listener);
-        this.client.dispatcher().executorService().shutdown();
+        LayoutInflater li = LayoutInflater.from(this);
+        View ipServerPopup = li.inflate(R.layout.server_ip_popup, null);
+        AlertDialog.Builder alertDialogBuilder = new AlertDialog.Builder(this);
+        alertDialogBuilder.setView(ipServerPopup);
+        EditText ipServerET = (EditText) ipServerPopup
+                .findViewById(R.id.etxt_server_ipaddress);
+        ipServerET.setText(serverIp);
+        alertDialogBuilder
+                .setCancelable(false)
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                serverIp=ipServerET.getText().toString();
+                                Request request = new Request.Builder().url("ws://"+serverIp+":8000/command/").build();
+                                Log.i("WS","request from address "+ipAddress);
+                                EchoWebSocketListener listener = new EchoWebSocketListener();
+                                webSocket = client.newWebSocket(request, listener);
+                                client.dispatcher().executorService().shutdown();
+                            }
+                        })
+                .setNegativeButton("Cancel",
+                        new DialogInterface.OnClickListener() {
+                            public void onClick(DialogInterface dialog,int id) {
+                                dialog.cancel();
+                                Intent intent = new Intent(getApplicationContext(), Starting.class);
+                                startActivity(intent);
+                            }
+                        });
+        AlertDialog alertDialog = alertDialogBuilder.create();
+        alertDialog.show();
+
         joystick.setOnMoveListener(new JoystickView.OnMoveListener() {
             @Override
             public void onMove(int angle, int strength) {
@@ -84,51 +103,30 @@ public class WSControl extends AppCompatActivity {
                 // define actions
                 if (strength>60&&(angle<=100 && angle>=80)){
                     // is up(90)  80<angle<100
-                    ws.send("{\"decision_\":\"FORWARD\"}");
+                    webSocket.send("{\"decision_\":\"FORWARD\"}");
                     txt_decision_value.setText("FORWARD");
                 }else if(strength>60&&(angle<=280 && angle>=260)){
                     // is down(270)  260<angle<280
-                    ws.send("{\"decision_\":\"BACKWARD\"}");
+                    webSocket.send("{\"decision_\":\"BACKWARD\"}");
                     txt_decision_value.setText("BACKWARD");
                 }else if(strength>60&&(angle>0 && angle<=10)){
                     // is right(0)  10<angle<350
-                    ws.send("{\"decision_\":\"RIGHT\"}");
+                    webSocket.send("{\"decision_\":\"RIGHT\"}");
                     txt_decision_value.setText("RIGHT");
                 }else if(strength>60&&(angle>=350 && angle<360)){
-                    ws.send("{\"decision_\":\"RIGHT\"}");
+                    webSocket.send("{\"decision_\":\"RIGHT\"}");
                     txt_decision_value.setText("RIGHT");
                 }else if(strength>60&&(angle<=190 && angle>=170)){
                     // is left(180)  170<angle<190
-                    ws.send("{\"decision_\":\"LEFT\"}");
+                    webSocket.send("{\"decision_\":\"LEFT\"}");
                     txt_decision_value.setText("LEFT");
                 }else if((angle==0&&strength==0)){
-                    ws.send("{\"decision_\":\"IDLE\"}");
+                    webSocket.send("{\"decision_\":\"IDLE\"}");
                     txt_decision_value.setText("IDLE");
                 }
 
             }
         });
-
-
-        handler = new Handler(Looper.getMainLooper()) {
-            @SuppressLint("SetTextI18n")
-            @Override
-            public void handleMessage(Message msg){
-                switch (msg.what){
-                    case CONNECTING_STATUS:
-                        switch(msg.arg1){
-                            case 1:
-                                Log.i("INFO","Connected to " + deviceName);
-                                break;
-                            case -1:
-                                Log.i("INFO","Device fails to connect");
-                                break;
-                        }
-                        break;
-
-                }
-            }
-        };
     }
     /* ============================ WS control =================================== */
     class EchoWebSocketListener extends WebSocketListener {
