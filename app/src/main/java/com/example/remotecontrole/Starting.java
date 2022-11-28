4fb5ef2c6dbe7340cccdf04e18e9381888ca2984
tarekbrahmi <brahmitarek6@gmail.com>
2022-11-28 09:10:30 +0100

    remove bluetooth rc

--- app/src/main/java/com/example/remotecontrole/Starting.java
+++ app/src/main/java/com/example/remotecontrole/Starting.java
@@ -6,24 +6,16 @@ import android.view.View;
 import android.widget.Button;
 
 public class Starting extends AppCompatActivity {
-    Button WsServerButton,PairDeviceButton;
+    Button WsServerButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_starting);
         WsServerButton=(Button)findViewById(R.id.btn_ws_server);// control via WS server
-        PairDeviceButton=(Button)findViewById(R.id.btn_pair_device);// list all paired device
         WsServerButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), WSControl.class);
-                startActivity(intent);
-            }
-        });
-        PairDeviceButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), DeviceSelect.class);
                 startActivity(intent);
             }
         });
