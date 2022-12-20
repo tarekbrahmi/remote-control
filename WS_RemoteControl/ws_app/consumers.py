1a51b921af0b4a52f8fbfc3c08ada3f4bdc6b49d
tarekbrahmi <brahmitarek6@gmail.com>
2022-12-20 16:20:58 +0100

    update pinout

--- WS_RemoteControl/ws_app/consumers.py
+++ WS_RemoteControl/ws_app/consumers.py
@@ -7,13 +7,13 @@ import subprocess
 
 class PINS:
     ######## PINS ########
-    EN_LEFT = 13
-    IN1 = 7
-    IN2 = 11
+    EN_LEFT = 2
+    IN1 = 3
+    IN2 = 4
 
-    EN_RIGHT = 12
-    IN3 = 8
-    IN4 = 10
+    EN_RIGHT = 17
+    IN3 = 27
+    IN4 = 22
 
 
 class DECISION:
@@ -36,7 +36,7 @@ class CommandConsumer(AsyncWebsocketConsumer):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         gpio.setmode(gpio.BCM)
-        gpio.setwarnings(False)
+        gpio.setwarnings(gpio.LOW)
         gpio.setmode(gpio.BOARD)
         gpio.setup(self.PINS.IN1, gpio.OUT)
         gpio.setup(self.PINS.IN2, gpio.OUT)
@@ -45,11 +45,11 @@ class CommandConsumer(AsyncWebsocketConsumer):
         #GPIO.PWM(channel, frequence)
         self.EN_RIGHT_PWM = gpio.PWM(self.PINS.EN_RIGHT, 100) 
         ##ici, rapport_cyclique vaut entre 0.0 et 100.0
-        self.EN_RIGHT_PWM.start(50) 
-        gpio.output(self.PINS.EN_RIGHT, gpio.HIGH)
+        self.EN_RIGHT_PWM.start(0) 
+        # gpio.output(self.PINS.EN_RIGHT, gpio.HIGH)
         self.EN_LEFT_PWM = gpio.PWM(self.PINS.EN_LEFT, 100)
-        self.EN_LEFT_PWM.start(50)
-        gpio.output(self.PINS.EN_LEFT, gpio.HIGH)
+        self.EN_LEFT_PWM.start(0)
+        # gpio.output(self.PINS.EN_LEFT, gpio.HIGH)
         print("set up the GPIO")
 
     async def connect(self):
@@ -68,7 +68,7 @@ class CommandConsumer(AsyncWebsocketConsumer):
                 'data': event['data']
             }))
 
-    async def handelCommand(self, command, vitess=0, exec=False):
+    async def handelCommand(self, command, vitess=0, exec=gpio.LOW):
         if str(command) == self.DECISION.FORWARD and not exec:
             self.forward(vitess=vitess)
         if str(command) == self.DECISION.BACKWARD and not exec:
@@ -112,50 +112,50 @@ class CommandConsumer(AsyncWebsocketConsumer):
         #TODO add the  ChangeDutyCycle to vitess
         self.EN_LEFT_PWM.ChangeDutyCycle(vitess)
         self.EN_RIGHT_PWM.ChangeDutyCycle(vitess)
-        gpio.output(self.PINS.IN1, False)
-        gpio.output(self.PINS.IN2, True)
-        gpio.output(self.PINS.IN3, True)
-        gpio.output(self.PINS.IN4, False)
+        gpio.output(self.PINS.IN1, gpio.HIGH)
+        gpio.output(self.PINS.IN2, gpio.LOW)
+        gpio.output(self.PINS.IN3, gpio.HIGH)
+        gpio.output(self.PINS.IN4, gpio.LOW)
         sleep(self.SLEEP_TIME)
         # self.clean()
 
     def backward(self, vitess: int):
         self.EN_LEFT_PWM.ChangeDutyCycle(vitess)
         self.EN_RIGHT_PWM.ChangeDutyCycle(vitess)
-        gpio.output(self.PINS.IN1, True)
-        gpio.output(self.PINS.IN2, False)
-        gpio.output(self.PINS.IN3, False)
-        gpio.output(self.PINS.IN4, True)
+        gpio.output(self.PINS.IN1, gpio.LOW)
+        gpio.output(self.PINS.IN2, gpio.HIGH)
+        gpio.output(self.PINS.IN3, gpio.LOW)
+        gpio.output(self.PINS.IN4, gpio.HIGH)
         sleep(self.SLEEP_TIME)
         # self.clean()
 
     def turn_right(self, vitess: int):
         self.EN_LEFT_PWM.ChangeDutyCycle(vitess)
         self.EN_RIGHT_PWM.ChangeDutyCycle(100-vitess)
-        gpio.output(self.PINS.IN1, False)
-        gpio.output(self.PINS.IN2, True)
-        gpio.output(self.PINS.IN3, False)
-        gpio.output(self.PINS.IN4, True)
+        gpio.output(self.PINS.IN1, gpio.HIGH)
+        gpio.output(self.PINS.IN2, gpio.LOW)
+        gpio.output(self.PINS.IN3, gpio.HIGH)
+        gpio.output(self.PINS.IN4, gpio.LOW)
         sleep(self.SLEEP_TIME)
         # self.clean()
 
     def turn_left(self, vitess: int):
         self.EN_LEFT_PWM.ChangeDutyCycle(100-vitess)
         self.EN_RIGHT_PWM.ChangeDutyCycle(vitess)
-        gpio.output(self.PINS.IN1, True)
-        gpio.output(self.PINS.IN2, False)
-        gpio.output(self.PINS.IN3, True)
-        gpio.output(self.PINS.IN4, False)
+        gpio.output(self.PINS.IN1, gpio.HIGH)
+        gpio.output(self.PINS.IN2, gpio.LOW)
+        gpio.output(self.PINS.IN3, gpio.HIGH)
+        gpio.output(self.PINS.IN4, gpio.LOW)
         sleep(self.SLEEP_TIME)
         # self.clean()
 
     def stop(self):
         self.EN_LEFT_PWM.ChangeDutyCycle(0)
         self.EN_RIGHT_PWM.ChangeDutyCycle(0)
-        gpio.output(self.PINS.IN1, False)
-        gpio.output(self.PINS.IN2, False)
-        gpio.output(self.PINS.IN3, False)
-        gpio.output(self.PINS.IN4, False)
+        gpio.output(self.PINS.IN1, gpio.LOW)
+        gpio.output(self.PINS.IN2, gpio.LOW)
+        gpio.output(self.PINS.IN3, gpio.LOW)
+        gpio.output(self.PINS.IN4, gpio.LOW)
         self.clean()
 
     async def receive(self, text_data):
