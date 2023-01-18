d267068d8514a9099846d36adf294432a9484efd
tarekbrahmi <brahmitarek6@gmail.com>
2023-01-18 22:35:13 +0100

    remove the async

--- WS_RemoteControl/ws_app/consumers.py
+++ WS_RemoteControl/ws_app/consumers.py
@@ -85,7 +85,7 @@ class CommandConsumer(AsyncWebsocketConsumer):
             self.stop()
         ###############################################
 
-    async def forward(self, vitess: int):
+    def forward(self, vitess: int):
         # TODO add the  ChangeDutyCycle to vitess-->DONE
         self.EN_LEFT_PWM.ChangeDutyCycle(vitess)
         self.EN_RIGHT_PWM.ChangeDutyCycle(vitess)
@@ -93,34 +93,34 @@ class CommandConsumer(AsyncWebsocketConsumer):
         gpio.output(self.PINS.IN2, gpio.LOW)
         gpio.output(self.PINS.IN3, gpio.HIGH)
         gpio.output(self.PINS.IN4, gpio.LOW)
-        await asyncio.sleep(self.SLEEP_TIME)
+        asyncio.sleep(self.SLEEP_TIME)
 
-    async def backward(self, vitess: int):
+    def backward(self, vitess: int):
         self.EN_LEFT_PWM.ChangeDutyCycle(vitess)
         self.EN_RIGHT_PWM.ChangeDutyCycle(vitess)
         gpio.output(self.PINS.IN1, gpio.LOW)
         gpio.output(self.PINS.IN2, gpio.HIGH)
         gpio.output(self.PINS.IN3, gpio.LOW)
         gpio.output(self.PINS.IN4, gpio.HIGH)
-        await asyncio.sleep(self.SLEEP_TIME)
+        asyncio.sleep(self.SLEEP_TIME)
 
-    async def turn_right(self, vitess: int):
+    def turn_right(self, vitess: int):
         self.EN_LEFT_PWM.ChangeDutyCycle(vitess)
         self.EN_RIGHT_PWM.ChangeDutyCycle(80)
         gpio.output(self.PINS.IN1, gpio.HIGH)
         gpio.output(self.PINS.IN2, gpio.LOW)
         gpio.output(self.PINS.IN3, gpio.HIGH)
         gpio.output(self.PINS.IN4, gpio.LOW)
-        await asyncio.sleep(self.SLEEP_TIME)
+        asyncio.sleep(self.SLEEP_TIME)
 
-    async def turn_left(self, vitess: int):
+    def turn_left(self, vitess: int):
         self.EN_LEFT_PWM.ChangeDutyCycle(80)
         self.EN_RIGHT_PWM.ChangeDutyCycle(vitess)
         gpio.output(self.PINS.IN1, gpio.HIGH)
         gpio.output(self.PINS.IN2, gpio.LOW)
         gpio.output(self.PINS.IN3, gpio.HIGH)
         gpio.output(self.PINS.IN4, gpio.LOW)
-        await asyncio.sleep(self.SLEEP_TIME)
+        asyncio.sleep(self.SLEEP_TIME)
 
     def stop(self):
         self.EN_LEFT_PWM.ChangeDutyCycle(0)
